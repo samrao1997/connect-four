183003fa42fd16e18857311ab959e23ae5dd7d01
Sam Rao <samrao1997@gmail.com>
2023-08-16 14:39:43 -0700

    main game loop

--- connect4.py
+++ connect4.py
@@ -1,9 +1,31 @@
 import numpy as np
 
+ROW_COUNT = 6
+COL_COUNT = 7
+
 
 def create_board():
-    board = np.zeros((6, 7))
+    board = np.zeros((ROW_COUNT, COL_COUNT))
     return board
+
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_location(board, col):
+    # the top row has a zero
+    return board[5][col] == 0
+
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+
+def print_board(board):
+    print(np.flip(board, 0))
 
 
 board = create_board()
@@ -14,11 +36,18 @@ turn = 0
 while not game_over:
     # Ask for player 1 input
     if turn == 0:
-        selection = int(input("Player 1 Make your Selection (0-6): "))
+        col = int(input("Player 1 Make your Selection (0-6): "))
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
 
     # Ask for player 2 input
     if turn == 1:
-        selection = int(input("Player 2 Make your Selection (0-6): "))
+        col = int(input("Player 2 Make your Selection (0-6): "))
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
 
+    print_board(board)
     turn += 1
     turn = turn % 2
