a3b42334f32dbfd9576f8bca4674d8dd95c8f648
Sam Rao <samrao1997@gmail.com>
2023-08-23 14:40:17 -0700

    Finish CL version of game

--- connect4.py
+++ connect4.py
@@ -15,7 +15,7 @@ def drop_piece(board, row, col, piece):
 
 def is_valid_location(board, col):
     # the top row has a zero
-    return board[5][col] == 0
+    return board[ROW_COUNT - 1][col] == 0
 
 
 def get_next_open_row(board, col):
@@ -28,11 +28,58 @@ def print_board(board):
     print(np.flip(board, 0))
 
 
+def winning_move(board, piece):
+    # TODO: MAKE Connect six ?
+    # check all horizontal locations for win
+    for c in range(COL_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if (
+                board[r][c] == piece
+                and board[r][c + 1] == piece
+                and board[r][c + 2] == piece
+                and board[r][c + 3] == piece
+            ):
+                return True
+
+    # Check vertical locations for win
+    for c in range(COL_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if (
+                board[r][c] == piece
+                and board[r + 1][c] == piece
+                and board[r + 2][c] == piece
+                and board[r + 3][c] == piece
+            ):
+                return True
+
+    # posively sloped diagonal
+    for c in range(COL_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if (
+                board[r][c] == piece
+                and board[r + 1][c + 1] == piece
+                and board[r + 2][c + 2] == piece
+                and board[r + 3][c + 3] == piece
+            ):
+                return True
+
+    # negatively sloped diagonal
+    for c in range(COL_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if (
+                board[r][c] == piece
+                and board[r - 1][c + 1] == piece
+                and board[r - 2][c + 2] == piece
+                and board[r - 3][c + 3] == piece
+            ):
+                return True
+
+
 board = create_board()
 game_over = False
 turn = 0
 
-
+print_board(board)
 while not game_over:
     # Ask for player 1 input
     if turn == 0:
@@ -41,12 +88,20 @@ while not game_over:
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
 
+            if winning_move(board, 1):
+                print("Player 1 wins. Congrats!")
+                game_over = True
+
     # Ask for player 2 input
     if turn == 1:
         col = int(input("Player 2 Make your Selection (0-6): "))
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+
+            if winning_move(board, 2):
+                print("Player 2 wins. Congrats!")
+                game_over = True
 
     print_board(board)
     turn += 1
